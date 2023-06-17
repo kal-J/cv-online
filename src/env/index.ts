ddfd8febade866b24fa76fdec41e46a486580663
kal-J <kharljay15@gmail.com>
2023-06-17 21:09:28 +0300

    fixing signup

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:1337/api/"
-export const DASHBOARD_URL = "http://localhost:1337/admin/"
+export const API_URL = "http://207.180.194.196:1337/api/"
+export const DASHBOARD_URL = "http://207.180.194.196:1337/admin/"
